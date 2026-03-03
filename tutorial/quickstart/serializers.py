75be8eaf153a6bcae729599dc1e61379dad3df60
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-03-03 16:34:09 +0200

    fix: change back to User and Group models contained by django.contrib.auth.models

--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -1,16 +1,17 @@
+from django.contrib.auth.models import Group, User
 from rest_framework import serializers
-from tutorial.quickstart.models import Company, User, Group
+from tutorial.quickstart.models import Company
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ["url", "username", "email", "groups", "company"]
+        fields = ["url", "username", "email", "groups"]
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
-        fields = ["url", "name", "employed"]
+        fields = ["url", "name"]
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
