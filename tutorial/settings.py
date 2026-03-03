d3a578be2c3875eb60d11371da6ad36bf14dbc03
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-03-03 13:51:50 +0200

    fix: allow all hosts to connect (not recommended for production)

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-38fuljo31(l322ok&c#h9kp(^q(bqdl&-#_gf5et2=0*jngrx%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
